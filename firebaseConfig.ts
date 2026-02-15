f9d183670c465fb7679a4a9aed5f8d97797641a9
abhi-0403 <bochareabhishek222@gmail.com>
2026-02-15 20:43:53 +0530

    Update firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -3,13 +3,13 @@ import { getDatabase } from "firebase/database";
 import type { FirebaseApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-C2FQkioCrUv6s6RY0srnuhsitg43R-g",
-  authDomain: "smartnest-13e23.firebaseapp.com",
-  databaseURL: "https://smartnest-13e23-default-rtdb.firebaseio.com",
-  projectId: "smartnest-13e23",
-  storageBucket: "smartnest-13e23.firebasestorage.app",
-  messagingSenderId: "232699275700",
-  appId: "1:232699275700:web:3233e410fde54314e32ae1",
+  apiKey: "YOUR FIREBASE API KEY",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Prevent re-initialization in development (Fast Refresh safe)
